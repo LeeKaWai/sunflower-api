9750c0bee9eeff469fcae0a776d9dad725d089a8
lijiawei <lijiawei@laihua.com>
2024-03-04 18:36:43 +0800

    first commit

---
+++ db/mongo.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	Client     *mongo.Client
+	Collection *mongo.Collection
+)
+
+func ConnectMongoDB() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Client = client
+	Collection = client.Database("sunflower").Collection("tasks")
+}
